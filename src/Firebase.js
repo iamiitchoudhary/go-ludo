0812ff32fc8e60f805ad30e8cc8938e0f1649055
iamiitchoudhary <ionfire111@gmail.com>
2025-06-10 01:57:15 +0530

    Add files via upload

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+// Firebase config placeholder
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
