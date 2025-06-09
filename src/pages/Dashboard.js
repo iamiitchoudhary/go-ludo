51283f1add97cd769f07afdf7db11f6cafab275a
iamiitchoudhary <ionfire111@gmail.com>
2025-06-10 01:53:47 +0530

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Game area or player stats */}
+    </div>
+  );
+}
+
+export default Dashboard;
